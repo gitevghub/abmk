efd53000f0136606ba79e0172a211cd409893059
gitevghub <99751044+gitevghub@users.noreply.github.com>
2023-03-07 09:14:23 +0200

    Update active.js

--- js/active.js
+++ js/active.js
@@ -4,17 +4,6 @@
 	
 			
 		/*====================================
-			Header Sticky JS
-		======================================*/ 
-		jQuery(window).on('scroll', function() {
-			if ($(this).scrollTop() > 1) {
-				$('.header').addClass("sticky");
-			} else {
-				$('.header').removeClass("sticky");
-			}
-		});
-		
-		/*====================================
 			Mobile Menu JS
 		======================================*/ 
 		$('.main-menu').slicknav({
@@ -24,20 +13,7 @@
 			easingOpen: "easeOutBounce",
 		});
 		
-		/*====================================
-			Search JS
-		======================================*/ 	
-		$('.top-search a').on( "click", function(){
-			$('.search-top').toggleClass('active');
-		});
-		
-		/*====================================
-			Team JS
-		======================================*/ 
-		$('.single-team').on( "click", function(){
-			$(this).toggleClass("active");
-		});
-		
+
 		/*====================================
 			Sidebar Popup JS
 		======================================*/ 	
@@ -49,171 +25,9 @@
 			$('.sidebar-popup').removeClass('active');
 		});
 		
-		/*====================================
-			Portfolio Details JS
-		======================================*/ 
-		$('.home-slider').owlCarousel({
-			items:1,
-			autoplay:false,
-			autoplayTimeout:5000,
-			smartSpeed: 400,
-			autoplayHoverPause:true,
-			loop:true,
-			merge:true,
-			nav:true,
-			dots:false,
-			navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-		});
-		
-		/*====================================
-			Portfolio Details JS
-		======================================*/ 
-		$('.pf-details-slider').owlCarousel({
-			items:1,
-			autoplay:false,
-			autoplayTimeout:5000,
-			smartSpeed: 400,
-			autoplayHoverPause:true,
-			loop:true,
-			merge:true,
-			nav:true,
-			dots:false,
-			navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-		});
-		
-		/*====================================
-			Portfolio Details JS
-		======================================*/ 
-		$('.testimonial-slider').owlCarousel({
-			items:1,
-			autoplay:false,
-			autoplayTimeout:5000,
-			smartSpeed: 400,
-			autoplayHoverPause:true,
-			loop:true,
-			merge:true,
-			nav:false,
-			dots:true,
-		});
 		
 		
-		/*====================================
-			Portfolio Details JS
-		======================================*/ 
-		$('.team-slider').owlCarousel({
-			items:3,
-			autoplay:false,
-			autoplayTimeout:5000,
-			smartSpeed: 400,
-			autoplayHoverPause:true,
-			loop:true,
-			merge:true,
-			nav:false,
-			dots:true,
-			responsive:{
-				300: {
-					items:1,
-				},
-				480: {
-					items:1,
-				},
-				768: {
-					items:2,
-				},
-				1170: {
-					items:3,
-				},
-			}
-		});
-		
-		/*====================================
-			Portfolio Details JS
-		======================================*/ 
-		$('.blog-latest-slider').owlCarousel({
-			items:2,
-			autoplay:false,
-			autoplayTimeout:5000,
-			smartSpeed: 400,
-			autoplayHoverPause:true,
-			loop:true,
-			merge:true,
-			nav:true,
-			navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-			dots:false,
-			responsive:{
-				300: {
-					items:1,
-				},
-				480: {
-					items:1,
-				},
-				768: {
-					items:2,
-				},
-				1170: {
-					items:2,
-				},
-			}
-		});
-		/*====================================
-			Portfolio Details JS
-		======================================*/ 
-		$('.partner-slider').owlCarousel({
-			items:6,
-			autoplay:false,
-			autoplayTimeout:5000,
-			smartSpeed: 400,
-			margin: 30,
-			autoplayHoverPause:true,
-			loop:true,
-			merge:true,
-			nav:true,
-			navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-			dots:false,
-			responsive:{
-				300: {
-					items:2,
-					nav:false,
-				},
-				480: {
-					items:3,
-					nav:false,
-				},
-				768: {
-					items:4,
-					nav:false,
-				},
-				1170: {
-					items:6,
-				},
-			}
-		});
-		
-		
-		
-		
-		
-		
-		
-		/*=====================================
-			CounterUp JS
-		======================================*/  
-		$('.number').counterUp({
-			time: 1000
-		});
-		
-		/*=====================================
-			Video Popup
-		======================================*/ 
-		$('.video-popup').magnificPopup({
-			type: 'iframe',
-			removalDelay: 300,
-			mainClass: 'mfp-fade'
-		});
-		
-	});
-	
-		
+
 		/*====================================
 			Scrool Up JS
 		======================================*/ 	
